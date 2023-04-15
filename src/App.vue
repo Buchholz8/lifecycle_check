e7a70d842f5ced0810273ccc3ae5ef666c01e848
markb <markbuchholz8@gmail.com>
2023-04-15 11:27:14 -0600

    third

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <life-cycle-example></life-cycle-example>
+  </div>
+</template>
+
+<script>
+import LifeCycleExample from './components/LifecycleExample.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LifeCycleExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LifecycleExample.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Totally A Title</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolorem,
+      culpa beatae, officia ea fugiat, officiis totam esse quisquam repudiandae
+      earum! Consequuntur commodi saepe nostrum impedit quidem et ipsam aperiam.
+    </p>
+    <h6>This was a cool thing i learned</h6>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, ut! Libero
+      reprehenderit, excepturi laudantium perferendis nihil recusandae
+      voluptate, illum alias consectetur quisquam vero nesciunt! Deleniti
+      corrupti possimus dolore non quidem?
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    beforeCreate: function() {
+        console.log(`this runs when 'before create' does`)
+    },
+    created: function() {
+        console.log(`this runs when 'created' does`)
+    },
+    beforeMount: function() {
+        console.log(`this runs when 'before mount' does`)
+    },
+    mounted: function() {
+        console.log(`this runs when 'mounted' does`)
+    }
+};
+</script>
+
+<style scoped>
+</style>
